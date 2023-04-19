b9da6fa485fe436a1b0d39b824382f8deddb0d72
eruxkatsumi <128930597+eruxkatsumi@users.noreply.github.com>
2023-04-18 21:35:32 -0400

    Update views.py
    metodo para enviar los datos del formulario hacia la base de datos

--- core/views.py
+++ core/views.py
@@ -29,3 +29,17 @@ def registro(request):
 
 def inicio_sesion(request):
     return render(request, 'core/inicio_sesion.html')
+
+def agregar_usuario(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        usuario = request.POST['usuario']
+        correo = request.POST['correo']
+        contrasena = request.POST['contrasena']
+        telefono = request.POST['telefono']
+        direccion = request.POST['direccion']
+        
+        if nombre and usuario and correo and contrasena and telefono and direccion
+        formulario = Formulario(nombre=nombre, usuario=usuario, correo=correo, contrasena=contrasena, telefono=telefono, direccion=direccion)
+        formulario.save()  
+    return render(request, 'core/inicio_sesion.html')
